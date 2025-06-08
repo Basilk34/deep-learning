d4eb19ec4413f7ba70fb69e0c1d1d63358bb4d59
Basilk34 <bmkanaan34@gmail.com>
2025-06-09 02:50:35 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,8 @@ import os
 file_id = "10jfNR3NcOh1MO2xpybPP9LrlRsTsHUzM"
 url = f"https://drive.google.com/uc?id={file_id}"
 output = "tokenizerrr.pkl"
-from tensorflow.keras.preprocessing.text import tokenizer_from_json
+import tensorflow.keras
+
 
 try:
     if not os.path.exists(output):
