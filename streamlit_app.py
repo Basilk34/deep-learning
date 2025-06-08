0847dc47737b89fa1288a6c23d7896af3ac77233
Basilk34 <bmkanaan34@gmail.com>
2025-06-09 00:54:53 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,6 @@ class AttentionLayer(Layer):
 YOUTUBE_API_KEY = "AIzaSyANEG0NbdmV_veIiZHY9cyK-0du_cYmtRk"
 TOKENIZER_PATH = "tokenizer.pkl"
 MODEL_PATH = "arabic_sentiment_cnn_lstm_att_20250605_1224.h5"
-
 LABELS = ['negative', 'neutral', 'positive']
 MAX_COMMENTS = 50
 
